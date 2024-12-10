ed64f978d3b06cc02685e147d77888af1b2e2bc1
niks3434 <surovikovav@gmail.com>
2024-12-11 00:05:29 +0300

    end

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,20 @@
-
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
+    document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('surnameOutput').innerText = initPerson.surname;
+    document.getElementById('patronymicOutput').innerText = initPerson.patronymic;
+    document.getElementById('birthYearOutput').innerText = initPerson.age;
 
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', function () {
+    document.getElementById('firstNameOutput').innerText = '';
+    document.getElementById('genderOutput').innerText = '';
+    document.getElementById('surnameOutput').innerText = '';
+    document.getElementById('patronymicOutput').innerText = '';
+    document.getElementById('birthYearOutput').innerText = '';
+});
+};
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,11 +35,49 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Анастасия",
+            "id_3": "Анна",
+            "id_4": "Ирина",
+            "id_5": "Вероника",
+            "id_6": "Таисия",
+            "id_7": "Ульяна",
+            "id_8": "Вера",
+            "id_9": "Надежда",
+            "id_10": "Любовь"
+        }
+    }`,
 
+
+    patronymicJson: `{
+        "count": 5,
+        "list": {     
+            "id_1": "Александрович",
+            "id_2": "Максимович",
+            "id_3": "Иванович",
+            "id_4": "Даниилович",
+            "id_5": "Андреевич"
+        } 
+    }`,
+    
+    
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
     randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
+
+    randomGender: function() {
+        return this.randomIntNumber() === 1 
+                        ? this.GENDER_FEMALE 
+                        : this.GENDER_MALE;  
+    },
+
+    randomAge: function() {
+        return this.randomIntNumber(2024, 1950);
+    },
 
     randomValue: function (json) {
         const obj = JSON.parse(json);
@@ -48,23 +86,36 @@ const personGenerator = {
     },
 
     randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+        return this.person.gender === this.GENDER_MALE 
+                        ? this.randomValue(this.firstNameMaleJson) 
+                        : this.randomValue(this.firstNameFemaleJson);
     },
 
 
-     randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+    randomSurname: function() {
+        const surname = this.randomValue(this.surnameJson);
+        return this.person.gender === this.GENDER_FEMALE 
+                        ? surname + "а" 
+                        : surname;
     },
+
+
+    randomPatronymic:  function() {
+        const patronymic = this.randomValue(this.patronymicJson);
+        return this.person.gender === this.GENDER_FEMALE 
+                        ? patronymic.slice(0, -2) + "на" 
+                        : patronymic;
+    },
+
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surname = this.randomSurname();
+        this.person.patronymic = this.randomPatronymic();
+        this.person.age = this.randomAge();
         return this.person;
     }
 };
